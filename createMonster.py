4cd87b260a7112bbc6f248d7da9cd9ea7017702e
SpeedyCatZz <112931053+SpeedyCatZz@users.noreply.github.com>
2022-09-09 15:44:49 +0200

    Ajout monstre

--- createMonster.py
+++ createMonster.py
@@ -2,6 +2,6 @@ from genMob import *
 import random
 
 def createMob():
-   listemob = ["KhaZix", "BelVeth", "ReqSai"]
+   listemob = ["KhaZix", "BelVeth", "ReqSai", "ChoGath"]
    nommob = random.choice (listemob)
    return genMob(nommob)
